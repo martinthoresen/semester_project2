4036a25384092acfba0b9c1d8e055ceb91446c34
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-02-26 11:59:41 +0100

    added local times to posts

--- src/js/feed/populateListing.js
+++ src/js/feed/populateListing.js
@@ -2,8 +2,14 @@ export const listingContainer = document.querySelector("#listings-container");
 
 export function populateListing(listing) {
   const singleListing = document.createElement("div");
+  const created = new Date(listing.created);
+  const localCreated = created.toLocaleString("no-NO", { timeZone: "UTC" });
+  const updated = new Date(listing.updated);
+  const localUpdated = created.toLocaleString("no-NO", { timeZone: "UTC" });
+  const endsAt = new Date(listing.endsAt);
+  const localEndsAt = created.toLocaleString("no-NO", { timeZone: "UTC" });
   listingContainer.appendChild(singleListing);
-  singleListing.innerHTML = `<div class="card p-3 mb-5 container" id="listing-card">
+  singleListing.innerHTML = `<div class="card  mb-5 " id="listing-card">
   <a href="/post/index.html?id=${listing.id}" class="text-decoration-none text-black">
   <div class="d-flex flex-column flex-md-row justify-content-between">
     <img class="listing-image" src="${listing.media}" />
@@ -13,6 +19,11 @@ export function populateListing(listing) {
       <p class="align-self-end">Tags: ${listing.tags}</p>
     </div>
     <div>
+      <div>
+      <p class="text-secondary">created: ${localCreated}</p>
+      <p class="text-secondary">updated: ${localUpdated}</p>
+      <p class="text-secondary">ends at: ${localEndsAt}</p>
+      </div>
       <p>Seller information:</p>
       <img class="seller-avatar" src="${listing.seller.avatar}" />
       <p>${listing.seller.name}</p>
