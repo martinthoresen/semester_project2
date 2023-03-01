e16e7f5231984fe55314d5f834654322d2bd9557
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-03-01 17:57:56 +0100

    create getUser function

---
+++ src/js/fetch/getUser.js
@@ -0,0 +1,12 @@
+export async function getUser(url) {
+  try {
+    const getData = {
+      method: "GET",
+    };
+    const response = await fetch(url, getData);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
