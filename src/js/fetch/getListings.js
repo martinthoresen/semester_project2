318d0d672a8b02909156f6e63f209478b18f635c
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-03-04 22:38:23 +0100

    added autorization

--- src/js/fetch/getListings.js
+++ src/js/fetch/getListings.js
@@ -1,7 +1,13 @@
+import { loadKey } from "../storage/local-storage.js";
+
 export async function getListings(url) {
+  const token = loadKey("accessToken");
   try {
     const getData = {
       method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     };
     const response = await fetch(url, getData);
     const json = await response.json();
