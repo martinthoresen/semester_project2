03441d6d6320172d49cc41225439800b21f725c0
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-03-04 22:37:50 +0100

    Update index.mjs

--- src/js/listing/index.mjs
+++ src/js/listing/index.mjs
@@ -1,2 +1,3 @@
 import * as populateSingleListing from "../listing/populateSingleListing.js";
 import * as showSingleListing from "../listing/showSingleListing.js";
+import * as makeBid from "../fetch/makeBid.js";
