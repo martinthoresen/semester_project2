2d6d71462569f2ec333b979b07bca28ddd4e41ec
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-03-04 22:38:12 +0100

    add authorization header on getuser function

--- src/js/fetch/getUser.js
+++ src/js/fetch/getUser.js
@@ -2,6 +2,9 @@ export async function getUser(url) {
   try {
     const getData = {
       method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     };
     const response = await fetch(url, getData);
     const json = await response.json();
